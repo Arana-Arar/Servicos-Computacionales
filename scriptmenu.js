9d21f3a99cc21043391d6d4c927daf7eb639f2be
Alan Arana <103553666+Arana-Arar@users.noreply.github.com>
2023-09-10 18:05:34 -0600

    Mi primer cambio

---
+++ scriptmenu.js
@@ -0,0 +1,9 @@
+<script>
+    const menuToggle = document.querySelector('.fa-bars');
+    const navMenu = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu');
+});
+
+  </script>
