fa375d42ffb962a1931b6df1b41852f901d3fa9d
Alan Arana <103553666+Arana-Arar@users.noreply.github.com>
2024-02-26 15:31:24 -0600

    nuevo

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,9 +1,14 @@
-<script>
-    const menuToggle = document.querySelector('.fa-bars');
-    const navMenu = document.querySelector('nav ul');
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
 
-menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('show-menu');
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
 });
-
-  </script>
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
